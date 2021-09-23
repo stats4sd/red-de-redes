9040abe9f5707269936598e96f95a627c75274c6
Dave Mills <d.e.mills@stats4sd.org>
2021-09-23 12:44:35 +0100

    make front + back end use same auth guard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -12,35 +12,47 @@ use DB;
 use Illuminate\Http\Request;
 use Khill\Lavacharts\Laravel\LavachartsFacade as Lava;
 use Khill\Lavacharts\Lavacharts;
+use stdClass;
 use Yajra\DataTables\DataTables;
 
 class DashboardController extends Controller
 {
     public function index ()
     {
+        // removing ref to db until we can sort out how to not make the mysql views take ages to load...
 
-        $years = Yearly::select('fecha')->orderBy('fecha')->get()->unique('fecha');
+        //$yearsdb = Yearly::select('fecha')->orderBy('fecha')->get()->unique('fecha');
+
+        //******* TEMP CODE TO GENERATE STATIC LIST OF YEARS *******/
+        $years = collect();
+
+        for($i = 2006; $i<2022; $i++) {
+            $newYear = new stdClass();
+            $newYear->fecha = $i;
+            $years->push($newYear);
+        }
+
         $stations = Station::orderBy('id')->get();
         return view('dashboard', compact('years', 'stations'));
     }
 
     public function charts(Request $request)
-    {   
+    {
         $id = $request->station_id;
         $aggr = $request->agg;
         $year = $request->year;
         $month = $request->month;
         if($aggr=='daily'){
-            $data = Daily::whereYear('fecha','=',$year)->whereMonth('fecha', $month)->where('id_station', $id)->orderBy('fecha')->get(); 
+            $data = Daily::whereYear('fecha','=',$year)->whereMonth('fecha', $month)->where('id_station', $id)->orderBy('fecha')->get();
         }elseif ($aggr == 'ten_days') {
-            $data = TenDays::whereYear('min_fecha','=',$year)->where('id_station', $id)->orderBy('min_fecha')->get(['min_fecha AS fecha', 'tendays_data.*']); 
+            $data = TenDays::whereYear('min_fecha','=',$year)->where('id_station', $id)->orderBy('min_fecha')->get(['min_fecha AS fecha', 'tendays_data.*']);
         }elseif ($aggr == 'monthly') {
-            $data = Monthly::where('year','=',$year)->orderBy('month')->where('id_station', $id)->get(); 
+            $data = Monthly::where('year','=',$year)->orderBy('month')->where('id_station', $id)->get();
         }elseif ($aggr == 'yearly') {
-            $data = Yearly::where('id_station', $id)->orderBy('fecha')->get(); 
+            $data = Yearly::where('id_station', $id)->orderBy('fecha')->get();
         }
 
-       
+
         return response()->json([
             'data' => $data
             ]);
@@ -50,5 +62,5 @@ class DashboardController extends Controller
 
 
 
-    
+
 }
