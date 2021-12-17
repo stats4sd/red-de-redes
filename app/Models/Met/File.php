5d8f5895c8c9f19fab5529506c383fa8e8866ff9
Dave Mills <d.e.mills@stats4sd.org>
2021-12-17 09:39:32 +0000

    updates to fix table and variable naming

--- app/Models/Met/File.php
+++ app/Models/Met/File.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace App;
+namespace App\Models\Met;
 
 use Illuminate\Database\Eloquent\Model;
 
 class File extends Model
 {
-    
+
 }
--- scripts/storeData.py
+++ scripts/storeData.py
@@ -18,8 +18,8 @@ try:
     # get pre-defined column name list
     cols = '`,`'.join(columns_name.list_columns_name)
 
-    # get all ID for this uploader_id in table data_template
-    sqlSelect = f"SELECT id FROM `data_template` WHERE `uploader_id`='{uploader_id}' ORDER BY id;"
+    # get all ID for this uploader_id in table met_data_preview
+    sqlSelect = f"SELECT id FROM `met_data_preview` WHERE `uploader_id`='{uploader_id}' ORDER BY id;"
 
     cursorSelect.execute(sqlSelect)
 
@@ -28,7 +28,7 @@ try:
     print("data is inserting")
 
     for row in resultSelect:
-        sqlInsert = f"INSERT INTO `data` (`{cols}`) SELECT `{cols}` FROM `data_template` WHERE `uploader_id`='{uploader_id}' AND `id` = {row[0]};"
+        sqlInsert = f"INSERT INTO `data` (`{cols}`) SELECT `{cols}` FROM `met_data_preview` WHERE `uploader_id`='{uploader_id}' AND `id` = {row[0]};"
 
         try:
             cursor.execute(sqlInsert)
@@ -47,7 +47,7 @@ except mysql.Error as err:
     print(f'Failed to upload data: {err}')
 
 else:
-    sqlDelete = f"DELETE FROM `data_template` WHERE `uploader_id`='{uploader_id}';"
+    sqlDelete = f"DELETE FROM `met_data_preview` WHERE `uploader_id`='{uploader_id}';"
     cursor.execute(sqlDelete)
     conn.commit()
     print('data inserted')
