6852c0f0341a72869da814bc2f8060ded1d3b5fc
Dan Tang <dan@stats4sd.org>
2022-01-20 14:27:53 +0000

    Add new item in main menu, to link to new data download page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -47,6 +47,9 @@
                           <a class="nav-link @if(Route::current()->uri==='home') active @endif" href="{{url('home')}}">Inicio</a>
                         </li>
                         <li class="nav-item">
+                          <a class="nav-link @if(Route::current()->uri==='data-download' && Auth::user()->type==="admin") active @endif" href="{{url('data-download')}}">Data Download</a>
+                        </li>
+                        <li class="nav-item">
                           <a class="nav-link @if(Route::current()->uri==='data-upload' && Auth::user()->type==="admin") active @endif" href="{{url('data-upload')}}">Subir Datos Meteorológicos</a>
                         </li>
                         <li class="nav-item">
