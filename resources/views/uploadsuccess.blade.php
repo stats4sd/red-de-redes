2d881e8d19c4b3e322d358c85a374568b25b3176
Dave Mills <d.e.mills@stats4sd.org>
2022-03-03 17:49:52 +0000

    update data uploader front-end to Vue3:  - replaced Bootstrap Vue components with alternatives (or vanilla html + bootstrap elements

--- resources/views/uploadsuccess.blade.php
+++ resources/views/uploadsuccess.blade.php
@@ -2,23 +2,23 @@
 @extends('layouts.app')
 @section('content')
 
- 
+
 
     <div class="mx-5">
-      <h3 class="section-title font-weight-bold text-center mb-3">Weatherstations</h3>
+        <h3 class="section-title font-weight-bold text-center mb-3">Weatherstations</h3>
         <p class="section-intro mx-auto text-center mb-5 text-secondary">Los datos han sido ingresados exitosamente..</p>
 
         <div class="row">
-						<div class="col-sm-2 offset-3">
-              <button type="button" class="btn btn-secondary" onclick="window.location.assign('/home')";><b>Go Back To Home Page</b></button>
-						</div>
-            <div class="col-sm-2 offset-2">
-              <button type="button" class="btn btn-secondary" onclick="window.location.assign('/data-upload')"><b>Upload Another Data File</b></button>
+            <div class="col-sm-2 offset-3">
+                <a type="button" class="btn btn-secondary" href="{{ url('home') }}"><b>Go Back To Home Page</b></a>
             </div>
-				</div>
+            <div class="col-sm-2 offset-2">
+                <a type="button" class="btn btn-secondary" href="{{ url('data-upload') }}"><b>Upload Another Data File</b></a>
+            </div>
+        </div>
 
     </div>
 
-    
+
 
 @endsection
