5fc64ae11accee68ed6d200a63a2464fd7527ae5
Dan Tang <dan@stats4sd.org>
2022-03-09 16:09:37 +0000

    1. Extract required code segment for data upload Vue component from app.js to a new file data_upload.js 2. Add data_upload.js in webpack.mix.js 3. Add javascript section to include library and custom Javascript file in dataupload.blade.php

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,3 @@
 require('./bootstrap')
 
 import { createApp } from 'vue'
-import UploaderWeatherData from "./components/UploaderWeatherData";
-
-createApp({
-    components: {
-        UploaderWeatherData
-    }
-}).mount('#app');
