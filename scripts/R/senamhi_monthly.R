14458b6a95a25f8d3cc40e055b8f428b84ed6f0b
ciara-mc <56551262+ciara-mc@users.noreply.github.com>
2022-03-18 15:34:52 +0000

    updates year and month

--- scripts/R/senamhi_monthly.R
+++ scripts/R/senamhi_monthly.R
@@ -22,7 +22,6 @@ con <- dbConnect(RMySQL::MySQL(),
 
 dbSendQuery(con, "set character set 'utf8mb4'")
 
-
 stations_table <- tbl(con, "stations") %>%
     select(1,3) %>%
     collect()
@@ -39,23 +38,31 @@ monthly_met_data_table <- tbl(con, "monthly_met_data")
 
 data <- monthly_met_data_table %>%
     mutate(
-        month = substring(as.character(year_and_month), 1, 4),
-        year = substring(as.character(year_and_month), 5, 6)
+        year = substring(as.character(year_and_month), 1, 4),
+        month = substring(as.character(year_and_month), 5, 6)
     ) %>%
-    filter(station_id == selected_station & year >= selected_start_year & year <= selected_end_year) %>%
+    filter(station_id == selected_station) %>%
     select(month, year, selected_variable) %>%
     collect()
 
+data <- data %>%
+        mutate(
+            year = as.numeric(year),
+            month = as.numeric(month)
+        ) %>%
+        filter(year >= selected_start_year & year <= selected_end_year)
+
 senamhi_monthly <- data %>%
     arrange(month) %>%
-    mutate(month = as.integer(month)) %>%
     complete(month = 1:12) %>%
     pivot_wider(names_from = "month", values_from = 3) %>%
+    complete(year = selected_start_year:selected_end_year) %>%
+    filter(!is.na(year)) %>%
     rename(
-        "AÑO" = year, ENE = `1`, FEB = `2`, MAR = `3`, ABR = `4`, MAY = `5`, JUN = `6`,
-        JUL = `7`, AGO = `8`, SEP = `9`, OCT = `10`, NOV = `11`, DIC = `12`
+      "AÑO" = year, ENE = `1`, FEB = `2`, MAR = `3`, ABR = `4`, MAY = `5`, JUN = `6`,
+      JUL = `7`, AGO = `8`, SEP = `9`, OCT = `10`, NOV = `11`, DIC = `12`
     )
-
+    
 senamhi_details <- paste(selected_station_label, selected_variable)
-
+    
 write.xlsx(senamhi_monthly, "senamhi_monthly.xlsx", sheetname = senamhi_details)
