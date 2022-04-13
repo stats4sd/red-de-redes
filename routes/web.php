7108d333eac8cf5a300f7ae5581c9b0cdc2603a0
Dan Tang <dan@stats4sd.org>
2022-04-13 16:09:03 +0100

    1. Change default page from intro page to login page 2. Remove link for Agronometric, change it to a non clickable label

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ use Symfony\Component\Process\Process;
 use Symfony\Component\Process\Exception\ProcessFailedException;
 
 Route::get('', function () {
-    return redirect('/intro');
+    return redirect('/login');
 });
 
 Auth::routes();
