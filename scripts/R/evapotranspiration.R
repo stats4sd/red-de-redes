e9795710505a082a29e9dc3ad2bf583d896e4e04
ciara-mc <56551262+ciara-mc@users.noreply.github.com>
2022-05-30 15:29:37 +0100

    sets up evapotranspiration script

---
+++ scripts/R/evapotranspiration.R
@@ -0,0 +1,28 @@
+library(dotenv)
+library(RMySQL)
+library(tidyverse)
+library(Evapotranspiration)
+
+dotenv::load_dot_env("../../.env")
+
+con <- dbConnect(RMySQL::MySQL(),
+                 dbname = Sys.getenv("DB_DATABASE"),
+                 host = Sys.getenv("DB_HOST"),
+                 port = as.integer(Sys.getenv("DB_PORT")),
+                 user = Sys.getenv("DB_USERNAME"),
+                 password = Sys.getenv("DB_PASSWORD")
+)
+
+daily_met_data_table <- tbl(con, "daily_met_data")
+
+data <- daily_met_data_table %>%
+          filter(is.na(evapo)) %>%
+          collect()
+
+data_updated <- data %>%
+                mutate(evapo=1)
+
+for(row in 1:nrow(data_updated)) {
+  query <- paste0("UPDATE daily_met_data SET evapo = ", data_updated$evapo[row], " WHERE id = '", data_updated$id[row], "';")
+  dbSendStatement(con, query)
+}
