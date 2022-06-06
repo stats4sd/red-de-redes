e0464f1b6db95683601f9d41927042015b1a3d74
ciara-mc <56551262+ciara-mc@users.noreply.github.com>
2022-06-06 15:34:30 +0100

    updates evapotranspirtaion

--- scripts/R/evapotranspiration.R
+++ scripts/R/evapotranspiration.R
@@ -17,17 +17,37 @@ met_data_table <- tbl(con, "met_data")
 
 data <- met_data_table %>%
           filter(is.na(evapo)) %>%
+          mutate(
+            Hour = sql(HOUR(fecha_hora)),
+            Day = sql(DAY(fecha_hora)),
+            Month = sql(MONTH(fecha_hora)),
+            Year = sql(YEAR(fecha_hora))
+          ) %>%
           collect()
 
-stations <- unique(data$station_id)
+data2 <- data %>%
+          rename(Station.Number = station_id,
+                 Temp = temperatura_externa,
+                 Tdew = punto_rocio,
+                 RH = humedad_externa,
+                 uz = velocidad_viento,
+                 Rs = solar_rad
+          ) %>%
+          select(id, Station.Number, Year, Month, Day, Hour, Temp, Tdew, RH, uz, Rs)
+
+stations <- unique(data2$station_id)
 
 for(i in 1:length(stations)) {
   
-  # update evapotranspiration calculation here
-  data_updated <- data %>% mutate(evapo=1)
+  # update evapotranspiration calculation here...
+
   
-  for(row in 1:nrow(data_updated)) {
-    query <- paste0("UPDATE daily_met_data SET evapo = ", data_updated$evapo[row], " WHERE station_id = '", stations[i], "' AND fecha = '", data_updated$fecha[row], "';")
+  evapo_results <- data.frame(results$ET.Daily) %>%
+    rownames_to_column("fecha") %>%
+    rename("evapo"="results.ET.Daily")
+  
+  for(row in 1:nrow(evapo_results)) {
+    query <- paste0("UPDATE daily_met_data SET evapo = ", evapo_results$evapo[row], " WHERE station_id = '", stations[i], "' AND fecha = '", evapo_results$fecha[row], "';")
     dbSendStatement(con, query)
   }
   
