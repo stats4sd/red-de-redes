71a40bbbe6b6730c0208b84dc5a509a59163ded7
Dave Mills <d.e.mills@stats4sd.org>
2022-07-08 16:38:53 +0100

    install horizon for job monitoring on server

--- config/app.php
+++ config/app.php
@@ -180,6 +180,7 @@ return [
         App\Providers\AuthServiceProvider::class,
         App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
+        App\Providers\HorizonServiceProvider::class,
         App\Providers\TelescopeServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         Khill\Lavacharts\Laravel\LavachartsServiceProvider::class,
