52f1b20b406b53106536d268ffea117bd9eb54a8
Dan Tang <dan@stats4sd.org>
2022-07-22 12:52:23 +0100

    data upload, other than daily met data, also generate tendays, monthly, yearly met data when user confirmed data upload

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -221,11 +221,31 @@ class FileController extends Controller
         $maxDate = (new Carbon($maxDate))->toDateString();
         $minDate = (new Carbon($minDate))->toDateString();
 
-        $result = \DB::select(
+        $maxMonth = substr($maxDate, 5, 2);
+        $minMonth = substr($minDate, 5, 2);
+
+        $maxYear = substr($maxDate, 0, 4);
+        $minYear = substr($minDate, 0, 4);
+
+        $dailyResult = \DB::select(
             "call generate_daily_met_data_by_date_range(?, ?, ?);",
             [$minDate, $maxDate, $fileRecord->station_id]
         );
 
+        $tendaysResult = \DB::select(
+            "call generate_tendays_met_data_by_year_range(?, ?, ?);",
+            [$minYear, $maxYear, $fileRecord->station_id]
+        );
+
+        $monthlyResult = \DB::select(
+            "call generate_monthly_met_data_by_month_range(?, ?, ?, ?, ?);",
+            [$minYear, $minMonth, $maxYear, $maxMonth, $fileRecord->station_id]
+        );
+
+        $yearlyResult = \DB::select(
+            "call generate_yearly_met_data_by_year_range(?, ?, ?);",
+            [$minYear, $maxYear, $fileRecord->station_id]
+        );
 
         Alert::add('success', "Carga completa. Todos los registros {$metDataCount} se almacenan en la base de datos y se han calculado los resúmenes diarios.")->flash();
 
