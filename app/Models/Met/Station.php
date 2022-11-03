dbe318c8be188b316347b33b7bfd6e02ba4fb152
ciara-mc <56551262+ciara-mc@users.noreply.github.com>
2022-11-03 11:48:03 +0000

    adds latest record field to stations crud

--- app/Models/Met/Station.php
+++ app/Models/Met/Station.php
@@ -42,4 +42,8 @@ class Station extends Model
         return $this->belongsTo(Organisation::class);
     }
 
+    public function getLatestRecordAttribute()
+    {
+        return $this->data()->orderByDesc('fecha_hora')->first()?->fecha_hora;
+    }
 }
